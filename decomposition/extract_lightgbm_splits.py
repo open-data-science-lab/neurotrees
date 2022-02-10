16dcd05dafb67a6a0399e9f0115e2028c771cb9f
rWX1047554 <radeev.nikita@huawei.com>
2022-02-10 17:06:05 +0700

    Implemented like a sklearn transformer

--- decomposition/extract_lightgbm_splits.py
+++ decomposition/extract_lightgbm_splits.py
@@ -1,9 +1,9 @@
 """
 Read docs here https://github.com/microsoft/LightGBM/blob/0688f471fc303af816cd6564107f6d720ebb99ec/python-package/lightgbm/basic.py#L3394
 """
-from pathlib import Path
 import pandas as pd
-import lightgbm as lgb
+
+from featuregenerator_lgbm import FeatureGeneratorLGBM
 
 df = pd.read_csv("decomposition/datasets/numerical/Breast_cancer_data.csv")
 X = df[['mean_radius','mean_texture','mean_perimeter','mean_area','mean_smoothness']]
@@ -11,26 +11,8 @@ y = df['diagnosis']
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
 
-lgbc = lgb.LGBMClassifier()
-
-print('Starting training...')
-lgbc.fit(X_train, y_train)
-y_pred = lgbc.predict(X_test)
-
-from sklearn.metrics import accuracy_score
-accuracy=accuracy_score(y_pred, y_test)
-print('LightGBM Model accuracy score: {0:0.4f}'.format(accuracy_score(y_test, y_pred)))
-print('Dumped model:')
-trees_df = lgbc.booster_.trees_to_dataframe()
-nodes_df = trees_df[trees_df['split_feature'].isna() == False] # if feature is None then it is a leaf
-feature_threshold_df = nodes_df[['split_feature', 'threshold']].reset_index(drop=True)
-nodes_num = len(feature_threshold_df)
-feature_generator_list = []
-for i in range(nodes_num):
-    feature_generator_list.append(lambda df: df[f'{feature_threshold_df.iloc[i][0]}'] <= feature_threshold_df.iloc[i][1])
-
-generated_df = pd.DataFrame()
-for i, generator in enumerate(feature_generator_list):
-    generated_df[f'{i}'] = generator(df)
+feature_generator = FeatureGeneratorLGBM()
+feature_generator.fit(X, y)
+generated_df = feature_generator.transform(X)
 
 print(generated_df)
---
+++ decomposition/featuregenerator_lgbm.py
@@ -0,0 +1,27 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
+import lightgbm as lgb
+
+class FeatureGeneratorLGBM(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.feature_generators_list = []
+        self.lgbc = lgb.LGBMClassifier()
+
+    def fit(self, X, y):
+        self.lgbc.fit(X, y)
+        trees_df = self.lgbc.booster_.trees_to_dataframe()
+        nodes_df = trees_df[trees_df['split_feature'].isna() == False] # if feature is None then it is a leaf
+        feature_threshold_df = nodes_df[['split_feature', 'threshold']].reset_index(drop=True)
+        nodes_num = len(feature_threshold_df)
+        for i in range(nodes_num):
+            self.feature_generators_list.append(lambda df: df[f'{feature_threshold_df.iloc[i][0]}'] <= feature_threshold_df.iloc[i][1])
+        return self
+
+    def transform(self, X: pd.DataFrame):
+        generated_df = pd.DataFrame()
+        for i, generator in enumerate(self.feature_generators_list):
+            generated_df[f'{i}'] = generator(X)
+        return generated_df
+
+    def get_lgbm_model(self):
+        return self.lgbc
